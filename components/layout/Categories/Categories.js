57bc29b1e88e00dc2410b064ef1f979634346127
Chimaobi Wogu <woguchimaobi@gmail.com>
2022-10-31 05:26:32 +0000

    categories

--- components/layout/Categories/Categories.js
+++ components/layout/Categories/Categories.js
@@ -1,7 +1,12 @@
+import { categories } from '../../../data/dummy';
+
 import styles from './Categories.module.css';
 
 
 const Categories = props => {
+
+    console.log(categories)
+    let boxStyle;
 
     return (
         <div className={styles.wrapper}>
@@ -9,15 +14,28 @@ const Categories = props => {
                 <h2>MAKE SOMEONE HAPPY WITH A GIFT</h2>
             </div>
             <div className={styles.content}>
-                <div className={styles.box1}>box one</div>
-                <div className={styles.box2}>2</div>
-                <div className={styles.box3}>3</div>
-                <div className={styles.box4}>4</div>
-                <div className={styles.box5}>5</div>
-                <div className={styles.box6}>6</div>
+                {categories.length !== 0 ? categories.map((category, index) => {
+                    boxStyle = 'box' + index;
+                    console.log(boxStyle)
+                    return <div className={`styles.${boxStyle}`}>{category.title}</div>
+                }) : ''}
+                {/* <div className={styles.box1}>123</div>
+                <div className={styles.box2}>123</div>
+                <div className={styles.box3}>123</div>
+                <div className={styles.box4}>123</div>
+                <div className={styles.box5}>123</div>
+                <div className={styles.box6}>123</div> */}
             </div>
         </div>
     )
 }
 
 export default Categories
+
+// style={{
+//     backgroundImage:
+//       `linear-gradient(to right, #ffffff49, #0000009c), url(${category.image})`,
+//     backgroundSize: "cover",
+//     backgroundPositionY: "30%",
+//     backgroundRepeat: "no-repeat",
+//     }}
---
+++ data/dummy.js
@@ -0,0 +1,50 @@
+
+const categories = [
+    {
+        title: 'Kitchen Items',
+        image: '/site/landing-pic1.png',
+    },
+    {
+        title: 'Gift Packaging',
+        image: '/site/sample1.jpg',
+    },
+    {
+        title: 'Bedsheets and Duvets',
+        image: '/site/landing-bedspread2.png',
+    },
+    {
+        title: 'Sourviners',
+        image: '/site/landing-pic1.png',
+    },
+    {
+        title: 'Car Accessories',
+        image: '/site/sample3.jpg',
+    },
+    {
+        title: 'Wedding Gifts',
+        image: '/site/sample4.jpg',
+    }
+] 
+
+const products = [
+    {
+        title: 'Hundreds of Household Items',
+        subText: 'Beautiful Artifacts',
+        image: '/site/landing-pic1.png',
+        buttonText: 'Shop Now'
+    },
+    {
+        title: 'Hundreds of Household Items',
+        subText: 'Beautiful Artifacts',
+        image: '/site/landing-bedspread.png',
+        buttonText: 'Shop Now'
+    },
+    {
+        title: 'Hundreds of Household Items',
+        subText: 'Beautiful Artifacts',
+        image: '/site/landing-bedspread2.png',
+        buttonText: 'Shop Now'
+    }
+]
+
+export { categories, products }
