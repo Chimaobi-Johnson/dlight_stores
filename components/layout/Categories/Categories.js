43fee26c72d6966972723592894ea1566c30f65b
Chimaobi Wogu <woguchimaobi@gmail.com>
2022-10-31 05:54:40 +0000

    styled categories section

--- components/layout/Categories/Categories.js
+++ components/layout/Categories/Categories.js
@@ -5,26 +5,24 @@ import styles from './Categories.module.css';
 
 const Categories = props => {
 
-    console.log(categories)
-    let boxStyle;
-
     return (
         <div className={styles.wrapper}>
             <div className={styles.heading}>
                 <h2>MAKE SOMEONE HAPPY WITH A GIFT</h2>
             </div>
             <div className={styles.content}>
-                {categories.length !== 0 ? categories.map((category, index) => {
-                    boxStyle = 'box' + index;
-                    console.log(boxStyle)
-                    return <div className={`styles.${boxStyle}`}>{category.title}</div>
+                {categories.length !== 0 ? categories.map(category => {
+                    return <div style={{
+                        backgroundImage:
+                          `linear-gradient(to right, #ffffff49, #0000009c), url(${category.image})`,
+                        backgroundSize: "cover",
+                        backgroundPositionY: "30%",
+                        backgroundRepeat: "no-repeat",
+                        }}>
+                            <h2 className={styles.categoryTitle}>{category.title} <div className={styles.underline}></div></h2>
+                           
+                        </div>
                 }) : ''}
-                {/* <div className={styles.box1}>123</div>
-                <div className={styles.box2}>123</div>
-                <div className={styles.box3}>123</div>
-                <div className={styles.box4}>123</div>
-                <div className={styles.box5}>123</div>
-                <div className={styles.box6}>123</div> */}
             </div>
         </div>
     )
@@ -32,10 +30,3 @@ const Categories = props => {
 
 export default Categories
 
-// style={{
-//     backgroundImage:
-//       `linear-gradient(to right, #ffffff49, #0000009c), url(${category.image})`,
-//     backgroundSize: "cover",
-//     backgroundPositionY: "30%",
-//     backgroundRepeat: "no-repeat",
-//     }}
