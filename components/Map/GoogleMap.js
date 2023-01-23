b95a002db104c52291c02ee487e3735252f2af11
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-01-23 22:36:41 +0000

    added google map component

---
+++ components/Map/GoogleMap.js
@@ -0,0 +1,160 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { createCustomEqual } from "fast-equals";
+import { isLatLngLiteral } from "@googlemaps/typescript-guards";
+const render = (status) => {
+  return <h1>{status}</h1>;
+};
+
+const GoogleMap = () => {
+  // [START maps_react_map_component_app_state]
+  const [clicks, setClicks] = React.useState([]);
+  const [zoom, setZoom] = React.useState(17); // initial zoom
+  const [center, setCenter] = React.useState({
+    lat: 4.834443012823783,
+    lng: 6.999846427555374,
+  });
+
+  const onClick = (e) => {
+    // avoid directly mutating state
+    setClicks([...clicks, e.latLng]);
+  };
+
+  const onIdle = (m) => {
+    console.log("onIdle");
+    setZoom(m.getZoom());
+    setCenter(m.getCenter().toJSON());
+  };
+
+  // [END maps_react_map_component_app_state]
+ 
+  // [START maps_react_map_component_app_return]
+  return (
+    <div style={{ display: "flex", height: "100%" }}>
+      <Wrapper apiKey={"AIzaSyBA4nd5u6cbOVHEY05loEEWCnAq9Lqq0I0"} render={render}>
+        <Map
+          center={center}
+          onClick={onClick}
+          onIdle={onIdle}
+          zoom={zoom}
+          style={{ flexGrow: "1", height: "100%" }}
+        >
+          {clicks.map((latLng, i) => (
+            <Marker key={i} position={latLng} />
+          ))}
+        </Map>
+      </Wrapper>
+    </div>
+  );
+  // [END maps_react_map_component_app_return]
+};
+
+const Map = ({ onClick, onIdle, children, style, ...options }) => {
+  // [START maps_react_map_component_add_map_hooks]
+  const ref = React.useRef(null);
+  const [map, setMap] = React.useState();
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+        console.log(window)
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+  // [END maps_react_map_component_add_map_hooks]
+  // [START maps_react_map_component_options_hook]
+  // because React does not do deep comparisons, a custom hook is used
+  // see discussion in https://github.com/googlemaps/js-samples/issues/946
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options);
+    }
+  }, [map, options]);
+  // [END maps_react_map_component_options_hook]
+  // [START maps_react_map_component_event_hooks]
+  React.useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) =>
+        google.maps.event.clearListeners(map, eventName)
+      );
+      if (onClick) {
+        map.addListener("click", onClick);
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+  }, [map, onClick, onIdle]);
+  // [END maps_react_map_component_event_hooks]
+  // [START maps_react_map_component_return]
+  return (
+    <>
+      <div ref={ref} style={style} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          // set the map prop on the child component
+          // @ts-ignore
+          return React.cloneElement(child, { map });
+        }
+      })}
+    </>
+  );
+  // [END maps_react_map_component_return]
+};
+
+// [START maps_react_map_marker_component]
+const Marker = (options) => {
+  const [marker, setMarker] = React.useState();
+
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new google.maps.Marker());
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+  return null;
+};
+
+const deepCompareEqualsForMaps = createCustomEqual((deepEqual) => (a, b) => {
+    if (
+      isLatLngLiteral(a) ||
+      a instanceof google.maps.LatLng ||
+      isLatLngLiteral(b) ||
+      b instanceof google.maps.LatLng
+    ) {
+      return new google.maps.LatLng(a).equals(new google.maps.LatLng(b));
+    }
+    // TODO extend to other types
+    // use fast-equals for other objects
+    return deepEqual(a, b);
+  });
+  
+  function useDeepCompareMemoize(value) {
+    const ref = React.useRef();
+  
+    if (!deepCompareEqualsForMaps(value, ref.current)) {
+      ref.current = value;
+    }
+    return ref.current;
+  }
+  
+  function useDeepCompareEffectForMaps(callback, dependencies) {
+    React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
+  }
+
+// [END maps_react_map_marker_component]
+
+export default GoogleMap
+
--- components/layout/Footer/Footer.js
+++ components/layout/Footer/Footer.js
@@ -1,55 +1,72 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
+import GoogleMap from "../../Map/GoogleMap";
 
+const Footer = (props) => {
 
-const Footer = props => {
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.footerTop}>
-                <div className={styles.box1}>
-                    <div className={styles.logoContainer}>
-                        <Image width={150} height={50} alt="" src='/logo/logo-light-text.png' />
-                    </div>
-                    {/* <p>Think marketing for grow and development</p> */}
-                </div>
-                <div className={styles.box2}>
-                    <h3>Quick Access</h3>
-                    <ul>
-                        <li>
-                            Home
-                        </li>
-                        <li>
-                            Products
-                        </li>
-                        <li>
-                            About us
-                        </li>
-                    </ul>
-                </div>
-                <div className={styles.box3}>
-                    <h3>Follow us</h3>
-                    <div className={styles.socialLinks}>
-                        <ul>
-                            <li><Image width={100} height={100} src="/icons/facebook-block.png" alt="" /></li>
-                            <li><Image width={100} height={100} src="/icons/instagram-block.png" alt="" /></li>
-                            <li><Image width={100} height={100} src="/icons/whatsapp-block.png" alt="" /></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className={styles.box4}>
-                    <h3>Call us</h3>
-                    <p>+234 7754212120</p>
-                </div>
+  return (
+      <div className={styles.wrapper}>
+        <div className={styles.footerTop}>
+          <div className={styles.box1}>
+            <div className={styles.mapContainer}>
+              <GoogleMap />
             </div>
-            <div className={styles.footerBottom}>
-                <div className={styles.copyright}>
-                    <p>&copy; Delight Stores | Powered by Delight Stores </p>
-                </div>
-                {/* <div className={styles.terms}><p>Terms and Conditions</p></div> */}
+            <p>Visit us at our store in Port Harcourt</p>
+          </div>
+          <div className={styles.box2}>
+            <h3>Quick Access</h3>
+            <ul>
+              <li>Home</li>
+              <li>Products</li>
+              <li>About us</li>
+            </ul>
+          </div>
+          <div className={styles.box3}>
+            <h3>Follow us</h3>
+            <div className={styles.socialLinks}>
+              <ul>
+                <li>
+                  <Image
+                    width={100}
+                    height={100}
+                    src="/icons/facebook-block.png"
+                    alt=""
+                  />
+                </li>
+                <li>
+                  <Image
+                    width={100}
+                    height={100}
+                    src="/icons/instagram-block.png"
+                    alt=""
+                  />
+                </li>
+                <li>
+                  <Image
+                    width={100}
+                    height={100}
+                    src="/icons/whatsapp-block.png"
+                    alt=""
+                  />
+                </li>
+              </ul>
             </div>
+          </div>
+          <div className={styles.box4}>
+            <h3>Call us</h3>
+            <p>+234 7754212120</p>
+          </div>
         </div>
-    )
-}
+        <div className={styles.footerBottom}>
+          <div className={styles.copyright}>
+            <p>&copy; Delight Stores | Powered by Delight Stores </p>
+          </div>
+          {/* <div className={styles.terms}><p>Terms and Conditions</p></div> */}
+        </div>
+      </div>
+  );
+  
+};
 
-export default Footer
+export default Footer;
