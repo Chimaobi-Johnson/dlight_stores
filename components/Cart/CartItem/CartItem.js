9cb172e26777f83a8d3e8312f387d68f4cc2be0a
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-01-26 22:22:38 +0000

    cart quantity button styling

--- components/Cart/CartItem/CartItem.js
+++ components/Cart/CartItem/CartItem.js
@@ -1,23 +1,23 @@
-import Input from '../../ui/Input/Input';
-import * as styles from './CartItem.module.css';
+import Input from "../../ui/Input/Input";
+import * as styles from "./CartItem.module.css";
 
-
-const CartItem = props => {
-
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.imageContainer}>
-
-            </div>
-            <div className={styles.contentContainer}>
-                <h4>Awesome pots for cooking</h4>
-                <span>2.5/6 Packs</span>
-                <div className={styles.inputContainer}>
-                 <Input type="text" defaultValue={0} label="quantity" />
-                </div>
-            </div>
+const CartItem = (props) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.imageContainer}></div>
+      <div className={styles.contentContainer}>
+        <h4>Awesome pots for cooking</h4>
+        <span>2.5/6 Packs</span>
+        <div className={styles.inputWrapper}>
+          <div className={styles.inputContainer}>
+            <div className={styles.decreaseBtn}>-</div>
+            <Input type="text" defaultValue={0} />
+            <div className={styles.increaseBtn}>+</div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default CartItem
+export default CartItem;
