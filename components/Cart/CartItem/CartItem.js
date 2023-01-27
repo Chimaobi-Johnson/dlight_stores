9809c2049259dfb9d78b15d10ff129eb4f8b23b9
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-01-27 19:41:32 +0000

    cart item styling

--- components/Cart/CartItem/CartItem.js
+++ components/Cart/CartItem/CartItem.js
@@ -1,12 +1,17 @@
+import Image from "next/image";
 import Input from "../../ui/Input/Input";
+import sampleImage from '../../../public/products/plastic-cups.png';
+
 import * as styles from "./CartItem.module.css";
 
 const CartItem = (props) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.imageContainer}></div>
+      <div className={styles.imageContainer}>
+       <Image width={100} height={100} src={sampleImage} alt="" />
+      </div>
       <div className={styles.contentContainer}>
-        <h4>Awesome pots for cooking</h4>
+        <h4 className={styles.heading4}>Awesome pots for cooking</h4>
         <span>2.5/6 Packs</span>
         <div className={styles.inputWrapper}>
           <div className={styles.inputContainer}>
@@ -15,6 +20,9 @@ const CartItem = (props) => {
             <div className={styles.increaseBtn}>+</div>
           </div>
         </div>
+      </div>
+      <div className={styles.close}>
+        <span>close</span>
       </div>
     </div>
   );
