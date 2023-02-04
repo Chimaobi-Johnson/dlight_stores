9d13c84bb4911231fd4450674295f2b02dbe573c
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-02-04 15:22:56 +0000

    added backend routes and axios

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,12 @@ import ProductInfo from "../components/Home/ProductInfo/ProductInfo";
 import Recommended from "../components/Home/Recommended/Recommended";
 import ImageSlider from "../components/ui/ImageSlider/ImageSlider";
 
+import axios from 'axios';
+
 import styles from "../styles/Home.module.css";
 
 function Home(props) {
+  // console.log(props)
   return (
     <BasicLayout
       metaData={{
@@ -26,9 +29,18 @@ function Home(props) {
 
 export async function getStaticProps() {
 
+  let products = null;
+  axios.get(process.env.BACKEND_URL + '/products')
+  .then(products => {
+    console.log(products)
+    products = products
+  }).catch(err => {
+    console.log(err)
+  })
+
   return {
     props: {
-      products: null,
+      products: products,
       categories: null
     },
     revalidate: 1
