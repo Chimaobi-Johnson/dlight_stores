86d4061c2ad6f8cff0965a39e6763ac8e2f22df7
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-02-09 14:52:14 +0000

    dynamic links for sidebar

--- components/layout/Sidebar/Sidebar.js
+++ components/layout/Sidebar/Sidebar.js
@@ -1,18 +1,36 @@
-import styles from './Sidebar.module.css';
+import Link from "next/link";
+import styles from "./Sidebar.module.css";
 
-const Sidebar = props => {
+const Sidebar = (props) => {
+  const { categories, currentCategory } = props;
 
-    const { categories, currentCategory } = props
+  return (
+    <div className={styles.wrapper}>
+      <ul>
+        {categories.length !== 0
+          ? categories.map((category, index) => {
+              return (
+                <Link
+                  key={index}
+                  href="/products/[id]"
+                  as={`/products/${category._id}`}
+                >
+                  <li
+                    className={
+                      category._id === currentCategory ? styles.active : null
+                    }
+                    key={index}
+                    id="linkItem"
+                  >
+                    {category.name}
+                  </li>
+                </Link>
+              );
+            })
+          : ""}
+      </ul>
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.wrapper}>
-            <ul>
-                {categories.length !== 0 ? categories.map((category, index) => {
-                    return <li className={category._id === currentCategory ? styles.active : null} key={index} id="linkItem">{category.name}</li>
-                }) : ''}
-            </ul>
-        </div>
-    )
-}
-
-export default Sidebar
+export default Sidebar;
