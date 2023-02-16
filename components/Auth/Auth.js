75c16cf14890b777954282359126afb94f3b46f3
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-02-16 18:04:07 +0000

    react-hook-form

--- components/Auth/Auth.js
+++ components/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import Button from '../ui/Button/Button';
 import Input from '../ui/Input/Input';
+import { useForm } from "react-hook-form";
 
 import axios from 'axios';
 
@@ -9,6 +10,9 @@ import styles from './Auth.module.css';
 
 
 const Auth = props => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
 
     const fromUrl = '' // get page user came from
 
@@ -69,36 +73,37 @@ const Auth = props => {
         })
     }
 
-    const registerUser = async () => {
-        if(inputData.password !== inputData.confirmPassword) {
-            alert('Password does not match')
-            return
-        }
-        const formData = new FormData()
-        formData.append('firstName', inputData.firstName)
-        formData.append('lastName', inputData.lastName)
-        formData.append('email', inputData.email)
-        formData.append('password', inputData.password)
+    const registerUser = async (data) => {
+        console.log(data)
+        // if(inputData.password !== inputData.confirmPassword) {
+        //     alert('Password does not match')
+        //     return
+        // }
+        // const formData = new FormData()
+        // formData.append('firstName', inputData.firstName)
+        // formData.append('lastName', inputData.lastName)
+        // formData.append('email', inputData.email)
+        // formData.append('password', inputData.password)
 
-        const result = await axios.post(process.env.NEXT_PUBLIC_BACKEND_URL + '/register', formData);
-        if(result.status === 200 || result.status === 201 ) {
-            alert('User created successfully')
-            setInputData(prevState => {
-                return {
-                    ...prevState,
-                    firstName: '',
-                    lastName: '',
-                    email: '',
-                    password: '',
-                    confirmPassword: ''
-                }
-            })
-            window.location.pathname = '/'
+        // const result = await axios.post(process.env.NEXT_PUBLIC_BACKEND_URL + '/register', formData);
+        // if(result.status === 200 || result.status === 201 ) {
+        //     alert('User created successfully')
+        //     setInputData(prevState => {
+        //         return {
+        //             ...prevState,
+        //             firstName: '',
+        //             lastName: '',
+        //             email: '',
+        //             password: '',
+        //             confirmPassword: ''
+        //         }
+        //     })
+        //     window.location.pathname = '/'
 
-        } else {
-            alert('Error')
-            console.log(result)
-        }
+        // } else {
+        //     alert('Error')
+        //     console.log(result)
+        // }
     }
     
 
@@ -116,13 +121,16 @@ const Auth = props => {
                     </>
                 ) : (
                     <>
+                     <form onSubmit={handleSubmit(registerUser)}>
                         <h1>Create account</h1>
-                        <Input type="text" value={inputData.firstName} onChange={(e) => changeTextHandler('firstName', e)} placeholder="First Name" label="First Name" />
+                        <Input type="text" {...register("firstName", { required: true })} value={inputData.firstName} onChange={(e) => changeTextHandler('firstName', e)} placeholder="First Name" label="First Name" />
                         <Input type="text" value={inputData.lastName} onChange={(e) => changeTextHandler('lastName', e)} placeholder="Last Name" label="Last Name" />
                         <Input type="email" value={inputData.email} onChange={(e) => changeTextHandler('email', e)} placeholder="Email" label="Email" />
                         <Input type="password" value={inputData.password} onChange={(e) => changeTextHandler('password', e)} placeholder="Password" label="Password" />
                         <Input type="password" value={inputData.confirmPassword} onChange={(e) => changeTextHandler('confirmPassword', e)} placeholder="Confirm Password" label="Confirm Password" />
-                        <Button onClick={registerUser} variant="secondary">SIGN UP</Button>
+                        <input type="submit" />
+                        {/* <Button type='submit' onSubmit={handleSubmit(registerUser)} variant="secondary">SIGN UP</Button> */}
+                    </form>
                         <p>Already have an account? <Link href="/auth/login">login here</Link></p>
                     </>
                 )}
