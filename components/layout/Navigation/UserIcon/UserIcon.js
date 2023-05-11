a8a0d4758b34b00a90974b71262994b7d58945ce
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-05-11 04:36:35 +0100

    styled user icon details box

--- components/layout/Navigation/UserIcon/UserIcon.js
+++ components/layout/Navigation/UserIcon/UserIcon.js
@@ -1,16 +1,28 @@
-import * as styles from './UserIcon.module.css';
+import Link from "next/link";
 
+import * as styles from "./UserIcon.module.css";
 
-const UserIcon = props => {
-    const firstLetter = props.firstName.split('')[0];
-    const secondLetter = props.lastName.split('')[0];
-    return (
-        <div className={styles.wrapper}>
-                <div className={styles.contentContainer}>
-                    <h4>{firstLetter + secondLetter}</h4>
-                </div>
-        </div>
-    )
-}
+const UserIcon = (props) => {
+  const firstLetter = props.firstName.split("")[0];
+  const secondLetter = props.lastName.split("")[0];
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.contentContainer}>
+        <h4>{firstLetter + secondLetter}</h4>
+      </div>
+      <div className={styles.detailsBoxContainer}>
+        <h3>{props.firstName + ' ' + props.lastName}</h3>
+        <ul>
+          <Link href="#">
+            <li>Account Details</li>
+          </Link>
+          <Link href="#">
+            <li>Log out</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default UserIcon
+export default UserIcon;
