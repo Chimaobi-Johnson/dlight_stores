39f33a11ec27d86a50a88a6c3189ac4cc5e74108
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-05-11 05:06:12 +0100

    added display logic to user details box

--- components/layout/Navigation/UserIcon/UserIcon.js
+++ components/layout/Navigation/UserIcon/UserIcon.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Link from "next/link";
 
 import * as styles from "./UserIcon.module.css";
@@ -5,21 +6,40 @@ import * as styles from "./UserIcon.module.css";
 const UserIcon = (props) => {
   const firstLetter = props.firstName.split("")[0];
   const secondLetter = props.lastName.split("")[0];
+
+  const [isOpen, setOpen] = useState(false)
+
+  const displayDetailsBox = () => {
+    setOpen(!isOpen);
+    const wrapper = document.getElementById("boxWrapper");
+    const content = document.getElementById("contentWrapper");
+
+    if (!isOpen) {
+      wrapper.style.height = "auto";
+      content.style.display = "block";
+    } else {
+      wrapper.style.height = "0";
+      content.style.display = "none";
+    }
+  };
+
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} onClick={() => displayDetailsBox()}>
       <div className={styles.contentContainer}>
         <h4>{firstLetter + secondLetter}</h4>
       </div>
-      <div className={styles.detailsBoxContainer}>
-        <h3>{props.firstName + ' ' + props.lastName}</h3>
-        <ul>
-          <Link href="#">
-            <li>Account Details</li>
-          </Link>
-          <Link href="#">
-            <li>Log out</li>
-          </Link>
-        </ul>
+      <div id="boxWrapper" className={styles.detailsBoxContainer}>
+        <div id="contentWrapper" className={styles.contentWrapper}>
+          <h3>{props.firstName + " " + props.lastName}</h3>
+          <ul>
+            <Link href="#">
+              <li>Account Details</li>
+            </Link>
+            <Link href="/log-out">
+              <li>Log out</li>
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
