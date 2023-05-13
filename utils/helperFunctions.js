4748a0949a50643f0243322c20d5b322a4d330ea
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-05-13 18:24:17 +0100

    filter function for cart items

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -14,10 +14,30 @@ export function logoutHandler() {
     });
     instance.get(process.env.NEXT_PUBLIC_BACKEND_URL + '/logout')
     .then(success => {
-        // reload
+    // reload
       window.location.reload()
     })
     .catch(err => {
       alert('error logging out Check connection settings')
     })
   }
+
+export function addCartItemsToLoggedUserCart (items) {
+
+    //check if item exists
+    currentCart = [{productId: 'sdsd'}]
+
+    const newArray = [];
+
+    items.map(item => {
+        newArray.push(currentCart.filter(checkProductId))
+        function checkProductId(productId) {
+            return productId === item
+        }
+    })
+
+
+
+    items.filter
+
+}
