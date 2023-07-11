a8e48953fd90b2ebe4aa58ed4ef607101985b715
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-07-11 01:23:45 +0100

    added back files after turning on server in heroku

---
+++ pages/products/index.js
@@ -0,0 +1,41 @@
+import Lists from '../../components/layout/Lists/Lists'
+import SidebarLayout from '../../components/layout/SidebarLayout/SidebarLayout'
+import * as flatted from 'flatted';
+
+import axios from 'axios'
+
+import { useDispatch } from 'react-redux';
+import { storeProducts } from '../../store/actions/products';
+
+export default function Products (props) {
+
+  const parsedProducts = flatted.parse(props.products)
+
+  const { products, categories } = props;
+
+  return (
+    <SidebarLayout metaData={{
+      title: "Products | Delight Stores",
+      description: "Everything household",
+      keywords: "gift items, online store, wedding gifts, souvenirs, household items"
+    }}>
+     <Lists categories={categories} list={parsedProducts} listName="product" /> 
+    </SidebarLayout>
+  )
+}
+
+
+export async function getStaticProps() {
+
+  const response = await axios.get(process.env.BACKEND_URL + '/products')
+  const response2 = await axios.get(process.env.BACKEND_URL + '/categories ')
+ 
+   return {
+     props: {
+       products: flatted.stringify(response.data.products),
+       categories: response2.data.categories
+     },
+     revalidate: 1
+   }
+ 
+ }
