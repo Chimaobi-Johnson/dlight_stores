976cd6f8d75017e28a1fa0fd8f0f2bbd7398d3c3
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-09-25 12:28:20 +0100

    animation for open and close payment container

--- components/OrderSummary/OrderSummary.js
+++ components/OrderSummary/OrderSummary.js
@@ -2,20 +2,39 @@ import * as styles from "./OrderSummary.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import Summary from "./Summary/Summary";
 import PaystackPage from "../CheckoutPage/PaystackPage/PaystackPage";
+import Button from "../ui/Button/Button";
 
 const OrderSummary = (props) => {
     const cartItems = useSelector((data) => data.app.cart.cartItems);
     const subTotal = useSelector((data) => data.app.cart.subTotal);
+
+    const initPaymentContainer = () => {
+        document.getElementById('contentWrapper').style.gridTemplateColumns = '1fr 1fr';
+        setTimeout(() => {
+            document.getElementById('paystackContainer').style.opacity = 1;
+        }, 1000);
+
+    }
+    const closePaymentContainer = () => {
+        document.getElementById('paystackContainer').style.opacity = 0;
+        setTimeout(() => {
+            document.getElementById('contentWrapper').style.gridTemplateColumns = '10fr 1fr';
+        }, 1000);
+
+    }
   return (
     <div className={styles.wrapper}>
-        <div className={styles.contentWrapper}>
+        <div id='contentWrapper' className={styles.contentWrapper}>
         <div className={styles.orderSummary}>
             <Summary cartItems={cartItems} subTotal={subTotal} />
+            <Button onClick={initPaymentContainer}>open</Button>
+            <Button onClick={closePaymentContainer}>close</Button>
+
         </div>
 
         <div className={styles.paymentDetails}>
-            
-        {/* <div className={styles.paystackContainer}>
+
+        <div id='paystackContainer' className={styles.paystackContainer}>
             <div className={styles.header}>
               <h1>Payment Details</h1>
               <p>Provide your payment details to complete your purchase</p>
@@ -24,7 +43,7 @@ const OrderSummary = (props) => {
                 cartItems={cartItems}
               />
             </div>
-          </div> */}
+          </div>
         </div>
         </div>
     </div>
