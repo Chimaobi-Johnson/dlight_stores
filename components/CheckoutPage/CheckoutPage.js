0b7b90fa9a8c50f90b273447e5e6f88e60600c97
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-09-25 05:36:01 +0100

    added login info component for checkout page

--- components/CheckoutPage/CheckoutPage.js
+++ components/CheckoutPage/CheckoutPage.js
@@ -6,18 +6,28 @@ import AddressForm from "./AddressForm/AddressForm";
 import PaystackPage from "./PaystackPage/PaystackPage";
 
 const CheckoutPage = (props) => {
-
   const cartItems = useSelector((data) => data.app.cart.cartItems);
   const subTotal = useSelector((data) => data.app.cart.subTotal);
 
-  const [deliveryType, setDeliveryType] = useState(null)
+  const [deliveryType, setDeliveryType] = useState(null);
 
   const changeInputHandler = (e) => {
-    setDeliveryType(e.target.value)
+    setDeliveryType(e.target.value);
+  };
+
+  const removeLoginInfo = () => {
+    document.getElementById('loginInfo').style.display = 'none'
   }
 
   return (
     <div className={styles.wrapper}>
+      <div id='loginInfo' className={styles.loginContainer}>
+        <span onClick={removeLoginInfo}>close {'(x)'}</span>
+        <h5>
+          You are not logged in, log in or register with us for an easier
+          experience
+        </h5>
+      </div>
       <div className={styles.contentWrapper}>
         <div className={styles.orderSummary}>
           <div className={styles.header}>
@@ -35,27 +45,30 @@ const CheckoutPage = (props) => {
           <div className={styles.deliveryMethodContainer}>
             <h3>Delivery Type</h3>
             <div>
-              <input onChange={(e) => changeInputHandler(e)} type="radio" id="pickup" name="deliveryType" value="pickup" />
+              <input
+                onChange={(e) => changeInputHandler(e)}
+                type="radio"
+                id="pickup"
+                name="deliveryType"
+                value="pickup"
+              />
               <label htmlFor="pickup">
                 Pick up - Order and pick up at our store any day you want
               </label>
               <br />
-              <input onChange={(e) => changeInputHandler(e)}
+              <input
+                onChange={(e) => changeInputHandler(e)}
                 type="radio"
                 id="delivery"
                 name="deliveryType"
                 value="delivery"
               />
-              <label htmlFor="delivery">
-                Delivery - (DHL)
-              </label>
+              <label htmlFor="delivery">Delivery - (DHL)</label>
               <br />
             </div>
           </div>
           <div className={styles.addressContainer}>
-                {deliveryType === 'delivery' ? (
-                    <AddressForm />
-                ): null}
+            {deliveryType === "delivery" ? <AddressForm /> : null}
           </div>
         </div>
 
@@ -63,11 +76,13 @@ const CheckoutPage = (props) => {
           <div className={styles.header}>
             <h1>Payment Details</h1>
             <p>Provide your payment details to complete your purchase</p>
-            <PaystackPage />
+            <PaystackPage
+              total={subTotal}
+              cartItems={cartItems}
+              deliveryType={deliveryType}
+            />
           </div>
-          <div className={styles.gatewayContainer}>
-         
-          </div>
+          <div className={styles.gatewayContainer}></div>
         </div>
       </div>
     </div>
