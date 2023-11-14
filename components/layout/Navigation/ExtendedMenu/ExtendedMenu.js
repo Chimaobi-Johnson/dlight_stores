5c6f49acf0d6d48491fe4e64b6116ec19ef7eb26
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-11-14 08:45:42 +0000

    extended menu recommendation

--- components/layout/Navigation/ExtendedMenu/ExtendedMenu.js
+++ components/layout/Navigation/ExtendedMenu/ExtendedMenu.js
@@ -3,11 +3,16 @@ import Image from 'next/image';
 
 import styles from './ExtendedMenu.module.css';
 import Link from 'next/link';
+import { useSelector } from 'react-redux';
 
 const ExtendedMenu = props => {
 
     const { openModal } = props
 
+    const products = useSelector(data => data.products.products)
+
+    const recommended = products && products.length !== 0 ? Math.round(Math.random() * products.length) : null
+    const recommended2 = products && products.length !== 0 ? Math.round(Math.random() * products.length) : null
 
     useEffect(() => {
 
@@ -43,18 +48,27 @@ const ExtendedMenu = props => {
                 </ul>
             </div>
             <div id="highlightedItems" className={styles.highlightedItems}>
-                <div className={styles.itemOneContainer}>
-                    <div className={styles.imageContainer}>
-                        <Image width={250} height={200} src="/products/others.png" alt="" />
-                    </div>
-                    <h6>Variety Packs</h6>
-                </div>
-                <div className={styles.itemTwoContainer}>
-                <div className={styles.imageContainer}>
-                        <Image width={250} height={200} src="/products/souviners.png" alt="" />
-                    </div>
-                    <h6>E-Gift Cards</h6>
-                </div>
+                {products && products.length !== 0 ? (
+                    <>
+                        <Link href={'/product/' + products[recommended]._id}>
+                            <div className={styles.itemOneContainer}>
+                                <div className={styles.imageContainer}>
+                                    <Image width={250} height={200} src={products[recommended].imagesUrl[0]} alt="" />
+                                </div>
+                                <h6>{products[recommended].name}</h6>
+                            </div>
+                        </Link>
+                        <Link href={'/product/' + products[recommended]._id}>
+                        <div className={styles.itemTwoContainer}>
+                        <div className={styles.imageContainer}>
+                                <Image width={250} height={200} src={products[recommended2].imagesUrl[0]} alt="" />
+                            </div>
+                            <h6>{products[recommended2].name}</h6>
+                        </div>
+                        </Link>
+                    </>
+                ) : <p>Loading...</p>}
+
             </div>
         </div>
     )
