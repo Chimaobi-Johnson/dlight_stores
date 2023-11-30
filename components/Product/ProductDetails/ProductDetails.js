7472fe36b0863dcc5cffdddfe239ccc3585c933a
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-11-30 13:11:09 +0000

    bug fix

--- components/Product/ProductDetails/ProductDetails.js
+++ components/Product/ProductDetails/ProductDetails.js
@@ -36,9 +36,6 @@ const ProductDetails = (props) => {
   } = props.product;
 
   console.log(props.product);
-  if (!props.product || props.product.length === 0) {
-    return <h1 style={{ textAlign: "center" }}>Product not found</h1>;
-  }
 
 
   const dispatch = useDispatch();
@@ -287,6 +284,11 @@ const ProductDetails = (props) => {
       />
     </div>
   );
+
+  if (!props.product || props.product.length === 0) {
+    return <h1 style={{ textAlign: "center" }}>Product not found</h1>;
+  }
+
 
 
   return (
