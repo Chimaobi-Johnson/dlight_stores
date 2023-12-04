d4e46b4545165d6083d7745a628dffb8fcc494b2
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-12-04 22:20:38 +0000

    added shipping locations to radio options

--- components/CheckoutPage/AddressForm/AddressForm.js
+++ components/CheckoutPage/AddressForm/AddressForm.js
@@ -6,15 +6,28 @@ import * as styles from "./AddressForm.module.css";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import { updateDeliveryDetails } from "../../../store/actions/app";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { isEmpty, logoutHandler } from "../../../utils/helperFunctions";
 
 const AddressForm = (props) => {
+
   const router = useRouter();
   const { register, reset, handleSubmit, formState: { errors } } = useForm({ defaultValues });
+  const content = useSelector(data => data.app.content)
   const dispatch = useDispatch()
+
+  const [shippingLocation, setShippingLocation] = useState(null);
+
+  const changeInputHandler = (e) => {
+    setShippingLocation(e.target.value);
+  };
+
   const submitFormHandler  = async (data) => {
-    dispatch(updateDeliveryDetails({...data, deliveryType: props.deliveryType}))
+    if(!shippingLocation) {
+      alert('Please select your shipping location')
+      return
+    }
+    dispatch(updateDeliveryDetails({...data, shippingLocation, deliveryType: props.deliveryType}))
     router.push('/order-summary')
   }
   const loggedUser = useSelector((data) => data.user);
@@ -37,14 +50,29 @@ const AddressForm = (props) => {
     if(!isEmpty(loggedUser)) {
       reset(defaultValues);
     }
-
-  
   }, [])
 
   return (
     <div className={styles.wrapper}>
-      <h3>Delivery Address</h3>
-      <span className={styles.info}>If you are outside Rivers state please contact us directly at inquiries@delighthomewarestores.com</span>
+      <h3>Select your area</h3>
+      <div className={styles.locationContainer}>
+        {content.shippingLocations.length !== 0 ? content.shippingLocations.map((item, index) => (
+          <div key={index}>
+             <input
+                type="radio"
+                id={item.locationName}
+                name="shippingLocation"
+                value={item}
+                onChange={(e) => changeInputHandler(e)}
+              />
+              <label htmlFor={item.locationName}>
+                {item.locationName} - N{item.locationPrice}
+              </label>
+              <br />
+          </div>
+        )) : ''}
+       </div>
+      <span className={styles.info}>If you are outside Rivers state please contact us directly at support@delighthomewarestores.com</span>
       <div className={styles.formContainer}>
         <Input
           type="text"
